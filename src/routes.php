aac2b8f6c0304aa67ea82b00f3c507893b8ce95a
Wilmer Márquez <gnumarquez@gmail.com>
2022-08-28 10:53:16 -0400

    manage-users

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web','auth'])->group(function()
+Route::middleware(['web','auth','can:manage users'])->group(function()
 {
 	Route::get('/users',\Gnumarquez\Http\Livewire\UsersComponent::class);
 });
